23ca601090549263ed64f19acdb55dc091d23a4c
egorivanov <egor.ivanov@pm.me>
2021-12-13 20:52:40 +0300

    updated tests and gitignore

--- src/test/java/io/qameta/allure/IssuesWebTest.java
+++ src/test/java/io/qameta/allure/IssuesWebTest.java
@@ -1,6 +1,11 @@
 package io.qameta.allure;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
 
 /**
  * @author eroshenkoam (Artem Eroshenko).
@@ -9,7 +14,32 @@ import org.junit.jupiter.api.Test;
 public class IssuesWebTest {
 
     @Test
+    @AllureId("90346")
+    @DisplayName("Creating new issue by authorized user")
+    @Tags({@Tag("web"), @Tag("regress"), @Tag("smoke")})
+    @Story("Create new issue")
+    @Owner("eroshenkoam")
+    @Feature("Issues")
     public void shouldCreateIssue() {
+
+        step("create web driver");
+
+        step("Open the main page", ()-> {
+            Allure.addAttachment("web page address", "https://github.com");
+        });
+        step("Create an issue", () -> {
+            step("click issues link on the menu");
+            step("Click new issue button");
+            step("add title");
+            step("add body");
+            step("click Submit new issue");
+        });
+        step("Check if issue has been created", () -> {
+            step("Check new ID created");
+            step("Check Title is present");
+        });
+        step("Stop web driver");
+
     }
 
 
