64c97ee0e7e078a6eb5b84e5b3bf953deec0f3ec
egorivanov <egor.ivanov@pm.me>
2021-12-13 18:55:33 +0300

    only template

--- src/test/java/io/qameta/allure/IssuesWebTest.java
+++ src/test/java/io/qameta/allure/IssuesWebTest.java
@@ -1,49 +1,16 @@
 package io.qameta.allure;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
 /**
  * @author eroshenkoam (Artem Eroshenko).
  */
-@Layer("web")
-@Owner("eroshenkoam")
-@Feature("Issues")
+
 public class IssuesWebTest {
 
-    private static final String OWNER = "allure-framework";
-    private static final String REPO = "allure2";
-
-    private static final String ISSUE_TITLE = "Some issue title here";
-
-    private final WebSteps steps = new WebSteps();
-
-    @BeforeEach
-    public void startDriver() {
-        steps.startDriver();
-    }
-
     @Test
-    @TM4J("AE-T3")
-    @Microservice("Billing")
-    @Story("Create new issue")
-    @JiraIssues({@JiraIssue("AE-8")})
-    @Tags({@Tag("web"), @Tag("critical")})
-    @DisplayName("Creating new issue by authorized user")
     public void shouldCreateIssue() {
-        steps.openIssuesPage(OWNER, REPO);
-        steps.createIssueWithTitle(ISSUE_TITLE);
-        steps.shouldSeeIssueWithTitle(ISSUE_TITLE);
     }
 
-
-    @AfterEach
-    public void stopDriver() {
-        steps.stopDriver();
-    }
 
 }
